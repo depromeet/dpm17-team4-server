f49fec74cb9c6fdcf54c26958321f58db61cdf1e
ihnokim <ihnokim58@gmail.com>
2025-08-19 02:53:41 +0900

    Add log configuration

---
+++ src/main/java/depromeet/lessonfour/server/config/RequestLoggingConfig.java
@@ -0,0 +1,20 @@
+package depromeet.lessonfour.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(1000);
+        filter.setIncludeHeaders(false);
+        return filter;
+    }
+}
