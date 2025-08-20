82505e69ddbfb4f7297b7fdb1420b1cc77e08729
ihnokim <ihnokim58@gmail.com>
2025-08-21 01:06:27 +0900

    Feature/basic devops (#11)
    * Add log configuration
    
    * Implement devops macros
    
    * Add spotless formatter
    
    * Implement format check workflow
    
    * Apply formatting
    
    * Update java version to 21
    
    * Ignore logs directory
    
    * Remove console log redirection
    
    * Enable payload logging
    
    * Add conditional logging
    
    * Fix bug
    
    * Apply formatting

--- src/test/java/depromeet/lessonfour/server/api/HelloControllerE2ETest.java
+++ src/test/java/depromeet/lessonfour/server/api/HelloControllerE2ETest.java
@@ -1,36 +1,36 @@
 package depromeet.lessonfour.server.api;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import io.restassured.RestAssured;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class HelloControllerE2ETest {
 
-    @LocalServerPort
-    private int port;
+  @LocalServerPort private int port;
 
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-    }
+  @BeforeEach
+  void setUp() {
+    RestAssured.port = port;
+    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+  }
 
-    @Test
-    void givenHelloController_whenHelloCalled_thenShouldReturnHelloWorldMessage() {
-        given()
-            .when()
-                .get("/api/hello")
-            .then()
-                .statusCode(200)
-                .contentType("text/plain;charset=UTF-8")
-                .body(equalTo("Hello, World!"));
-    }
+  @Test
+  void givenHelloController_whenHelloCalled_thenShouldReturnHelloWorldMessage() {
+    given()
+        .when()
+        .get("/api/hello")
+        .then()
+        .statusCode(200)
+        .contentType("text/plain;charset=UTF-8")
+        .body(equalTo("Hello, World!"));
+  }
 }
