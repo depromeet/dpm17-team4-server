87f52f7fcd556fe39cfe36a55769ef4a219ae96b
Koo <-->
2025-08-24 20:01:16 +0900

    [#16] feat : 토큰 재발급 API 추가

--- src/main/java/depromeet/lessonfour/server/auth/api/AuthController.java
+++ src/main/java/depromeet/lessonfour/server/auth/api/AuthController.java
@@ -1,15 +1,20 @@
 package depromeet.lessonfour.server.auth.api;
 
+import depromeet.lessonfour.server.auth.api.dto.request.ReIssueRequestDto;
+import depromeet.lessonfour.server.auth.api.dto.request.RegisterRequestDto;
+import depromeet.lessonfour.server.auth.api.dto.response.AccessTokenResponseDto;
+import depromeet.lessonfour.server.auth.service.ReIssueTokenUseCase;
+import depromeet.lessonfour.server.auth.service.RegisterUseCase;
+import depromeet.lessonfour.server.auth.service.dto.ReIssueResult;
+import depromeet.lessonfour.server.common.utils.HttpServletUtils;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import depromeet.lessonfour.server.auth.api.dto.request.RegisterRequestDto;
-import depromeet.lessonfour.server.auth.service.RegisterUseCase;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,10 +22,21 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
   private final RegisterUseCase registerUseCase;
+  private final ReIssueTokenUseCase reIssueTokenUseCase;
+  private final HttpServletUtils servletUtils;
 
   @PostMapping("/register")
   public ResponseEntity<?> register(@Valid @RequestBody RegisterRequestDto dto) {
     registerUseCase.register(dto);
     return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/reissue")
+  public ResponseEntity<?> reIssue(@Valid @RequestBody ReIssueRequestDto dto, HttpServletResponse response) {
+    ReIssueResult result = reIssueTokenUseCase.reIssue(dto);
+
+    servletUtils.addCookie(response, HttpServletUtils.REFRESH_TOKEN_COOKIE_NAME, result.refreshToken(), HttpServletUtils.REFRESH_TOKEN_EXPIRATION);
+
+    return ResponseEntity.ok(new AccessTokenResponseDto(result.accessToken()));
   }
 }
--- src/main/java/depromeet/lessonfour/server/auth/config/SecurityConfig.java
+++ src/main/java/depromeet/lessonfour/server/auth/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(
             auth ->
-                auth.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/auth/register")
+                auth.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/auth/register", "/api/auth/reissue")
                     .permitAll()
                     .anyRequest()
                     .authenticated())
