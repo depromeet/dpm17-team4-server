80e4d39213cdfcebb472ac37b0f7cf3e249cbd6c
ihnokim <ihnokim58@gmail.com>
2025-09-12 01:59:41 +0900

    Replace with snake case

--- src/main/java/depromeet/lessonfour/server/auth/security/jwt/JwtConstants.java
+++ src/main/java/depromeet/lessonfour/server/auth/security/jwt/JwtConstants.java
@@ -5,7 +5,7 @@ import java.time.Duration;
 public interface JwtConstants {
 
   Duration REFRESH_TOKEN_EXPIRATION = Duration.ofDays(7);
-  String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
+  String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
   String BEARER_PREFIX = "Bearer ";
   String AUTHORIZATION_HEADER = "Authorization";
 }
