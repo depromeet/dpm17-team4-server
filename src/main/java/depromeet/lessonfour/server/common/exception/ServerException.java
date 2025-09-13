4a9bc199a277f2429067f3eb3e8d466e86175fa2
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-09-13 19:40:00 +0900

    [#15] feat: exception 및 공통 응답 구조 setting

---
+++ src/main/java/depromeet/lessonfour/server/common/exception/ServerException.java
@@ -0,0 +1,14 @@
+package depromeet.lessonfour.server.common.exception;
+
+import depromeet.lessonfour.server.common.exception.base.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
+public class ServerException extends RuntimeException {
+	private final BaseErrorCode baseErrorCode;
+
+	public ServerException(BaseErrorCode baseErrorCode) {
+		super(baseErrorCode.getMessage());
+		this.baseErrorCode = baseErrorCode;
+	}
+}
---
+++ src/main/java/depromeet/lessonfour/server/common/exception/base/BaseErrorCode.java
@@ -0,0 +1,9 @@
+package depromeet.lessonfour.server.common.exception.base;
+
+import org.springframework.http.HttpStatus;
+
+public interface BaseErrorCode {
+	HttpStatus getHttpStatus();
+
+	String getMessage();
+}
