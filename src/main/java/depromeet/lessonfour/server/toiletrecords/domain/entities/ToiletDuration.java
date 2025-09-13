5f11b965db724278fbc3649f3b35b3021c07c127
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-09-13 19:45:49 +0900

    [#15] feat: entity 세팅

---
+++ src/main/java/depromeet/lessonfour/server/toiletrecords/domain/entities/ToiletDuration.java
@@ -0,0 +1,12 @@
+package depromeet.lessonfour.server.toiletrecords.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ToiletDuration {
+	WITHIN_FIVE_MINUTES,
+	WITHIN_TEN_MINUTES,
+	OVER_TEN_MINUTES
+}
