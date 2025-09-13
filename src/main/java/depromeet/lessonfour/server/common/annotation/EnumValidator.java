788d5df7f82df8e388bcdf133556d0437bcd3662
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-09-13 23:32:54 +0900

    [#15] refactor: EnumValidator 수정

--- src/main/java/depromeet/lessonfour/server/common/annotation/EnumValidator.java
+++ src/main/java/depromeet/lessonfour/server/common/annotation/EnumValidator.java
@@ -9,17 +9,21 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class EnumValidator implements ConstraintValidator<ValidEnum, Enum<?>> {
   private Set<String> validValues;
+  private Class<? extends Enum<?>> enumClass;
 
   @Override
   public void initialize(ValidEnum annotation) {
+    this.enumClass = annotation.enumClass();
     validValues =
-        Arrays.stream(annotation.enumClass().getEnumConstants())
+        Arrays.stream(this.enumClass.getEnumConstants())
             .map(Enum::name)
             .collect(Collectors.toSet());
   }
 
   @Override
   public boolean isValid(Enum<?> value, ConstraintValidatorContext context) {
-    return value != null && validValues.contains(value.name());
+    return value != null
+        && value.getDeclaringClass().equals(enumClass)
+        && validValues.contains(value.name());
   }
 }
