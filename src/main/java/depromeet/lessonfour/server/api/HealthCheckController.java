3294322616b0b9f1146aa360fa9bc40901bbad92
Koo <-->
2025-09-13 20:23:23 +0900

    [#43] feat(HealthCheckController) : json 응답으로 수정

--- src/main/java/depromeet/lessonfour/server/api/HealthCheckController.java
+++ src/main/java/depromeet/lessonfour/server/api/HealthCheckController.java
@@ -1,15 +1,18 @@
 package depromeet.lessonfour.server.api;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/health")
+@RequestMapping("/api/v1/healthcheck")
 public class HealthCheckController {
 
   @GetMapping
-  public String healthCheck() {
-    return "OK";
+  public ResponseEntity<Map<String, String>> healthCheck() {
+    return ResponseEntity.ok(Map.of("status", "UP"));
   }
 }
