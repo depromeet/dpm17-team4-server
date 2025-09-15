78cee70bf35c12aa98e685bd78d4170fec41aaa9
Koo <-->
2025-09-15 12:01:36 +0900

    [#47] fix : 생활 기록 생성 음식 validation 수정

--- src/main/java/depromeet/lessonfour/server/activityrecords/schemas/request/CreateActivityRecordsRequest.java
+++ src/main/java/depromeet/lessonfour/server/activityrecords/schemas/request/CreateActivityRecordsRequest.java
@@ -1,14 +1,12 @@
 package depromeet.lessonfour.server.activityrecords.schemas.request;
 
-import java.util.List;
-import java.util.UUID;
-
 import depromeet.lessonfour.server.activityrecords.domain.entities.MealTime;
 import depromeet.lessonfour.server.activityrecords.domain.entities.StressLevel;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
 
 public record CreateActivityRecordsRequest(
     @Valid List<CreateFoodRequestDto> foods,
@@ -16,6 +14,6 @@ public record CreateActivityRecordsRequest(
     @NotNull StressLevel stressLevel) {
 
   public record CreateFoodRequestDto(
-      @NotBlank(message = "음식 id는 필수입니다") UUID foodId,
+      @NotNull(message = "음식 id는 필수입니다") UUID foodId,
       @NotNull(message = "식사 시간은 필수입니다") MealTime mealTime) {}
 }
