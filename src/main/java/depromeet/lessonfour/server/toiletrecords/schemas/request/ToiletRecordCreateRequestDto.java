181b705c6ec730e1aebde76aedffb3f2fffffd23
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-09-17 17:05:07 +0900

    [#48] chore(ToiletRecordCreateRequestDto): 리뷰 반영 annotation 및 오타 수정

--- src/main/java/depromeet/lessonfour/server/toiletrecords/schemas/request/ToiletRecordCreateRequestDto.java
+++ src/main/java/depromeet/lessonfour/server/toiletrecords/schemas/request/ToiletRecordCreateRequestDto.java
@@ -8,14 +8,14 @@ import depromeet.lessonfour.server.toiletrecords.domain.entities.ToiletShape;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ToiletRecordCreateRequestDto(
-    @NotBlank(message = "배변시각 기록은 필수입니다.") LocalDateTime selectedWhen,
-    @NotBlank(message = "배변 여부 기록은 필수입니다.") boolean selectedSuccess,
+    @NotNull(message = "배변시각 기록은 필수입니다.") LocalDateTime selectedWhen,
+    @NotNull(message = "배변 여부 기록은 필수입니다.") boolean selectedSuccess,
     @ValidEnum(enumClass = ToiletColor.class, message = "유효하지 않은 배변 색깔입니다.")
         ToiletColor selectedColor,
-    @ValidEnum(enumClass = ToiletShape.class, message = "유효하지 않은 배변 색깔입니다.")
+    @ValidEnum(enumClass = ToiletShape.class, message = "유효하지 않은 배변 형태입니다.")
         ToiletShape selectedShape,
     @Min(value = 0, message = "복통 점수는 0 이상이어야 합니다.")
         @Max(value = 100, message = "복통 점수는 100 이하여야 합니다.")
