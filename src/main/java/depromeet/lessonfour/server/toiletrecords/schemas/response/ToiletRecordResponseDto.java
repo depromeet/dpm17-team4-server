19744cba9adea25a8b3b99e82591095ac5befb8f
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-09-17 16:37:52 +0900

    [#48] feat: ToiletRecord CRUD dto 생성

---
+++ src/main/java/depromeet/lessonfour/server/toiletrecords/schemas/response/ToiletRecordResponseDto.java
@@ -0,0 +1,35 @@
+package depromeet.lessonfour.server.toiletrecords.schemas.response;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import depromeet.lessonfour.server.toiletrecords.domain.entities.ToiletColor;
+import depromeet.lessonfour.server.toiletrecords.domain.entities.ToiletRecord;
+import depromeet.lessonfour.server.toiletrecords.domain.entities.ToiletShape;
+
+public record ToiletRecordResponseDto(
+	UUID id,
+	UUID userId,
+
+	LocalDateTime selectedWhen,
+	boolean selectedSuccess,
+	ToiletColor selectedColor,
+	ToiletShape selectedShape,
+	int selectedPain,
+	int selectedTimeTaken,
+	String selectedOptional
+) {
+	public static ToiletRecordResponseDto of(ToiletRecord entity) {
+		return new ToiletRecordResponseDto(
+			entity.getId(),
+			entity.getUser().getId(),
+			entity.getToiletAt(),
+			entity.isToiletSuccess(),
+			entity.getToiletColor(),
+			entity.getToiletShape(),
+			entity.getPainScore(),
+			entity.getToiletDuration(),
+			entity.getAdditionalNote()
+		);
+	}
+}
