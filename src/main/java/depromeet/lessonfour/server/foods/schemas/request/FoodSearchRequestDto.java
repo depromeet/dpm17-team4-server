76bc2ca48e62f0c8ffc41a7e15a63a6b96d58819
ihnokim <ihnokim58@gmail.com>
2025-09-18 18:36:35 +0900

    Apply formatting

--- src/main/java/depromeet/lessonfour/server/foods/schemas/request/FoodSearchRequestDto.java
+++ src/main/java/depromeet/lessonfour/server/foods/schemas/request/FoodSearchRequestDto.java
@@ -4,6 +4,4 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record FoodSearchRequestDto(
-    @NotBlank String query,
-    @NotNull @Min(0) Integer count) {}
+public record FoodSearchRequestDto(@NotBlank String query, @NotNull @Min(0) Integer count) {}
