984fb2415d1574aae5c65f4e8bd750b657d50bc7
ihnokim <ihnokim58@gmail.com>
2025-09-18 10:57:58 +0900

    Rename field

--- src/main/java/depromeet/lessonfour/server/foods/schemas/response/FoodDto.java
+++ src/main/java/depromeet/lessonfour/server/foods/schemas/response/FoodDto.java
@@ -2,8 +2,6 @@ package depromeet.lessonfour.server.foods.schemas.response;
 
 import java.util.UUID;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,7 +10,5 @@ import lombok.Getter;
 public class FoodDto {
   private UUID id;
   private String name;
-
-  @JsonProperty("score")
-  private double foodScore;
+  private double score;
 }
