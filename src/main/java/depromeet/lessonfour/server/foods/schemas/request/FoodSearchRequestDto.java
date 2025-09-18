7821efa4c1035855952e80b081aa64496cd84a08
ihnokim <ihnokim58@gmail.com>
2025-09-18 10:54:27 +0900

    Remove no arg constructors to enhance validation

--- src/main/java/depromeet/lessonfour/server/foods/schemas/request/FoodSearchRequestDto.java
+++ src/main/java/depromeet/lessonfour/server/foods/schemas/request/FoodSearchRequestDto.java
@@ -2,10 +2,8 @@ package depromeet.lessonfour.server.foods.schemas.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class FoodSearchRequestDto {
   private String query;
