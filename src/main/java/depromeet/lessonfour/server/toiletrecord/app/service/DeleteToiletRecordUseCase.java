8936af7c5961cf04a841fa4139ced16fbd8952e2
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-09-24 05:07:02 +0900

    [#53] feat(DeleteToiletRecordUseCase): 배변기록 삭제(Soft delete) UseCase 추가

---
+++ src/main/java/depromeet/lessonfour/server/toiletrecord/app/service/DeleteToiletRecordUseCase.java
@@ -0,0 +1,26 @@
+package depromeet.lessonfour.server.toiletrecord.app.service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import depromeet.lessonfour.server.common.annotation.UseCase;
+import depromeet.lessonfour.server.common.api.code.ErrorCode;
+import depromeet.lessonfour.server.common.exception.ServerException;
+import depromeet.lessonfour.server.toiletrecord.app.repository.ToiletRecordRepository;
+import depromeet.lessonfour.server.toiletrecord.domain.entity.ToiletRecord;
+import lombok.RequiredArgsConstructor;
+
+@UseCase
+@Transactional
+@RequiredArgsConstructor
+public class DeleteToiletRecordUseCase {
+
+  private final ToiletRecordRepository toiletRecordRepository;
+
+  public void delete(Long userId, Long recordId) {
+    ToiletRecord record =
+        toiletRecordRepository
+            .findByUser_IdAndIdAndIsDeletedFalse(userId, recordId)
+            .orElseThrow(() -> new ServerException(ErrorCode.DATA_NOT_FOUND));
+    record.delete();
+  }
+}
