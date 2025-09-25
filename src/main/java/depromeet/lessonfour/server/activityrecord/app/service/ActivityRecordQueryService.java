385857a2ca7f4dec24d319c9ae26ef0a0c5a4909
Koo <koosco136@gmail.com>
2025-09-25 17:21:25 +0900

    [#65] fix : ActivityRecordQueryService readOnly 추가

--- src/main/java/depromeet/lessonfour/server/activityrecord/app/service/ActivityRecordQueryService.java
+++ src/main/java/depromeet/lessonfour/server/activityrecord/app/service/ActivityRecordQueryService.java
@@ -1,19 +1,19 @@
 package depromeet.lessonfour.server.activityrecord.app.service;
 
+import depromeet.lessonfour.server.activityrecord.app.repository.ActivityRecordRepository;
+import depromeet.lessonfour.server.activityrecord.domain.entity.ActivityRecord;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import org.springframework.stereotype.Service;
-
-import depromeet.lessonfour.server.activityrecord.app.repository.ActivityRecordRepository;
-import depromeet.lessonfour.server.activityrecord.domain.entity.ActivityRecord;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ActivityRecordQueryService {
 
