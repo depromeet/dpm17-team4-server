199aed35f293a9e80de654db14bcec625a57fe47
Koo <koosco136@gmail.com>
2025-09-25 17:37:57 +0900

    [#65] style : code formatting

--- src/main/java/depromeet/lessonfour/server/activityrecord/app/service/ActivityRecordQueryService.java
+++ src/main/java/depromeet/lessonfour/server/activityrecord/app/service/ActivityRecordQueryService.java
@@ -1,16 +1,18 @@
 package depromeet.lessonfour.server.activityrecord.app.service;
 
-import depromeet.lessonfour.server.activityrecord.app.repository.ActivityRecordRepository;
-import depromeet.lessonfour.server.activityrecord.domain.entity.ActivityRecord;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import depromeet.lessonfour.server.activityrecord.app.repository.ActivityRecordRepository;
+import depromeet.lessonfour.server.activityrecord.domain.entity.ActivityRecord;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional(readOnly = true)
