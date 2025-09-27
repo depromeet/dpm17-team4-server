0df1b5a3c265af6554d7ccb6627827ced0e75841
onlyshyun <mn031621019@gmail.com>
2025-09-27 11:06:21 +0900

    fix(server): REFRESH_TOKEN httpOnly false

--- src/main/java/depromeet/lessonfour/server/auth/api/util/RefreshTokenCookieGenerator.java
+++ src/main/java/depromeet/lessonfour/server/auth/api/util/RefreshTokenCookieGenerator.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseCookie;
 public class RefreshTokenCookieGenerator {
   public static ResponseCookie generate(String refreshToken) {
     return ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, refreshToken)
-        .httpOnly(true)
+        .httpOnly(false)
         .sameSite("None")
         .maxAge(7 * 24 * 60 * 60)
         .path("/")
