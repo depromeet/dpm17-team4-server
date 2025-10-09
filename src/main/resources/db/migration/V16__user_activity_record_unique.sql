2113ad5e134b4f3820d50182aa9477dc09e226c4
Koo <koosco136@gmail.com>
2025-10-10 02:37:59 +0900

    [#79] add conditional unique constraint on activity_record for non-deleted entries

--- src/main/resources/db/migration/V16__user_activity_record_unique.sql
+++ src/main/resources/db/migration/V16__user_activity_record_unique.sql
@@ -1,2 +1,3 @@
 alter table activity_record
-    add constraint uk_user_date unique (user_id, activity_date);
+    add constraint uk_user_date unique (user_id, activity_date)
+    where is_deleted = false;
