a78b337e64ddffdac97d41d0afd0213b865f69c7
Koo <koosco136@gmail.com>
2025-10-11 22:01:09 +0900

    [#79] add null handling and equality check in ActivityAt for improved safety

--- src/main/java/depromeet/lessonfour/server/common/domain/vo/ActivityAt.java
+++ src/main/java/depromeet/lessonfour/server/common/domain/vo/ActivityAt.java
@@ -11,11 +11,13 @@ import jakarta.annotation.Nullable;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@EqualsAndHashCode
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
 public class ActivityAt {
@@ -23,11 +25,19 @@ public class ActivityAt {
   private LocalDate date;
   private LocalTime time;
 
+  private static final ActivityAt EMPTY = new ActivityAt(LocalDate.MIN, LocalTime.MIN);
+
   public static ActivityAt of(LocalDate date) {
+    if (date == null) {
+      return EMPTY;
+    }
     return new ActivityAt(date, LocalTime.MIDNIGHT); // 기본 시간을 자정으로 설정
   }
 
   public static ActivityAt from(LocalDateTime dateTime) {
+    if (dateTime == null) {
+      return EMPTY;
+    }
     return new ActivityAt(dateTime.toLocalDate(), dateTime.toLocalTime());
   }
 
@@ -48,7 +58,7 @@ public class ActivityAt {
   }
 
   public List<LocalDate> datesUntil(@Nullable ActivityAt end) {
-    if (end == null) {
+    if (end == null || end.equals(EMPTY)) {
       return List.of(date);
     }
 
