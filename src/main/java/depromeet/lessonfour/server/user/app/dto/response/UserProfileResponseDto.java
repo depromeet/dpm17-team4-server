dbb04d3abbe4757a7b9533e7ab7e65438e995e4b
Koo <koosco136@gmail.com>
2025-10-12 21:22:11 +0900

    [#69] change User entity path

--- src/main/java/depromeet/lessonfour/server/user/app/dto/response/UserProfileResponseDto.java
+++ src/main/java/depromeet/lessonfour/server/user/app/dto/response/UserProfileResponseDto.java
@@ -1,6 +1,6 @@
 package depromeet.lessonfour.server.user.app.dto.response;
 
-import depromeet.lessonfour.server.user.domain.entity.User;
+import depromeet.lessonfour.server.user.domain.User;
 
 public record UserProfileResponseDto(Long id, String email, String nickname, String profileImage) {
 
