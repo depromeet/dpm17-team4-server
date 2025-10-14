ece2a92f5bd3059698e09c223d682629b4ea66d9
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-10-15 00:44:54 +0900

    [#81] feat(ToiletRecordRepository): 날짜별 시간 목록 조회 시그니처 추가

--- src/main/java/depromeet/lessonfour/server/toiletrecord/domain/repository/ToiletRecordRepository.java
+++ src/main/java/depromeet/lessonfour/server/toiletrecord/domain/repository/ToiletRecordRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import depromeet.lessonfour.server.common.domain.view.DailyExistenceView;
+import depromeet.lessonfour.server.common.domain.view.RecordTimeView;
 import depromeet.lessonfour.server.common.domain.vo.ActivityAt;
 import depromeet.lessonfour.server.toiletrecord.domain.entity.ToiletRecord;
 
@@ -14,6 +15,8 @@ public interface ToiletRecordRepository {
   Optional<ToiletRecord> findByUserAndId(Long userId, Long recordId);
 
   List<ToiletRecord> findByDate(Long userId, ActivityAt at);
+
+  List<RecordTimeView> findTimesByDate(Long userId, ActivityAt activityAt);
 
   DailyExistenceView existsByActivityAt(Long userId, ActivityAt at);
 
