b96cc06d900503712119ebd08ea754c67f81c1cc
ihnokim <ihnokim58@gmail.com>
2025-10-19 12:17:06 +0900

    Feature/logout (#87)
    * Implement logout
    
    * Fix flyway migration error
    
    * Fix invalid import statement

--- src/main/java/depromeet/lessonfour/server/auth/infra/security/config/SecurityConfig.java
+++ src/main/java/depromeet/lessonfour/server/auth/infra/security/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
     return http.securityMatcher("/swagger-ui/**", "/v3/api-docs/**", "")
         .csrf(AbstractHttpConfigurer::disable)
         .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
+        .logout(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
         .build();
   }
@@ -65,6 +66,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .logout(AbstractHttpConfigurer::disable)
         .httpBasic(AbstractHttpConfigurer::disable)
         .anonymous(AbstractHttpConfigurer::disable)
         .formLogin(AbstractHttpConfigurer::disable)
@@ -109,6 +111,7 @@ public class SecurityConfig {
                     .authenticated())
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .logout(AbstractHttpConfigurer::disable)
         .addFilterBefore(
             new JwtAuthenticationFilter(authenticationManager),
             UsernamePasswordAuthenticationFilter.class)
--- src/test/java/depromeet/lessonfour/server/recordquery/api/CalendarControllerE2ETest.java
+++ src/test/java/depromeet/lessonfour/server/recordquery/api/CalendarControllerE2ETest.java
@@ -37,7 +37,7 @@ import depromeet.lessonfour.server.toiletrecord.domain.repository.ToiletRecordRe
 import depromeet.lessonfour.server.toiletrecord.domain.vo.ToiletColor;
 import depromeet.lessonfour.server.toiletrecord.domain.vo.ToiletShape;
 import depromeet.lessonfour.server.user.domain.entity.User;
-import depromeet.lessonfour.server.user.infra.repository.UserRepository;
+import depromeet.lessonfour.server.user.domain.repository.UserRepository;
 import io.restassured.RestAssured;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
