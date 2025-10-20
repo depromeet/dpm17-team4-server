0984ac1da28de024ffe6ffc86006366f8a169e8a
ihnokim <ihnokim58@gmail.com>
2025-10-21 00:25:46 +0900

    Fix syntax error

--- src/main/resources/db/migration/V18__create_notification_settings.sql
+++ src/main/resources/db/migration/V18__create_notification_settings.sql
@@ -1,12 +1,12 @@
 CREATE TABLE notification_settings (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT NOT NULL,
-    `key` VARCHAR(32) NOT NULL,
+    key VARCHAR(32) NOT NULL,
     registration_token VARCHAR(512) NOT NULL UNIQUE,
     enabled BOOLEAN NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT uk_notification_settings_user_id_key UNIQUE (user_id, `key`)
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uk_notification_settings_user_id_key UNIQUE (user_id, key)
 );
 
 CREATE INDEX idx_notification_settings_user_id ON notification_settings(user_id);
