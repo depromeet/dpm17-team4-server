03cb758fc4c5f7a0a87c70a20f5ee2ccbded0bb6
ihnokim <ihnokim58@gmail.com>
2025-10-21 01:23:07 +0900

    Remove snake case strategy

--- src/main/java/depromeet/lessonfour/server/notification/app/dto/request/SaveNotificationSettingsRequestDto.java
+++ src/main/java/depromeet/lessonfour/server/notification/app/dto/request/SaveNotificationSettingsRequestDto.java
@@ -1,14 +1,10 @@
 package depromeet.lessonfour.server.notification.app.dto.request;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record SaveNotificationSettingsRequestDto(
     @Schema(description = "기기 식별자", example = "my_iphone")
         @NotBlank(message = "기기 식별자는 필수입니다")
--- src/main/java/depromeet/lessonfour/server/notification/app/dto/request/UpdateNotificationSettingsRequestDto.java
+++ src/main/java/depromeet/lessonfour/server/notification/app/dto/request/UpdateNotificationSettingsRequestDto.java
@@ -1,12 +1,8 @@
 package depromeet.lessonfour.server.notification.app.dto.request;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record UpdateNotificationSettingsRequestDto(
     @Schema(description = "기기 식별자 (선택)", example = "my_iphone")
         @Size(max = 32, message = "기기 식별자는 최대 32자까지 가능합니다")
