3e0fbe52820d5f526ff50644dd4bda9b1ca51823
Koo <koosco136@gmail.com>
2025-10-21 21:35:44 +0900

    add common event schema

---
+++ src/main/java/depromeet/lessonfour/server/common/domain/event/CommonEvent.java
@@ -0,0 +1,12 @@
+package depromeet.lessonfour.server.common.domain.event;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record CommonEvent<T>(
+    UUID eventId,
+    String eventType,
+    String source,
+    LocalDateTime occurredAt,
+    String traceId,
+    T data) {}
