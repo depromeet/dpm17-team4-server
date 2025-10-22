73687c39e71f2f5622e77d56b150b0e88d238912
Koo <koosco136@gmail.com>
2025-10-22 16:20:23 +0900

    refactor ReportClientImpl to use ToiletScoreService instead of ReportQueryService

--- src/main/java/depromeet/lessonfour/server/recordquery/infra/ReportClientImpl.java
+++ src/main/java/depromeet/lessonfour/server/recordquery/infra/ReportClientImpl.java
@@ -4,17 +4,17 @@ import org.springframework.stereotype.Component;
 
 import depromeet.lessonfour.server.common.domain.vo.ActivityAt;
 import depromeet.lessonfour.server.recordquery.app.client.ReportClient;
-import depromeet.lessonfour.server.report.app.service.ReportQueryService;
+import depromeet.lessonfour.server.report.app.service.ToiletScoreService;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class ReportClientImpl implements ReportClient {
 
-  private final ReportQueryService reportQueryService;
+  private final ToiletScoreService toiletScoreService;
 
   @Override
   public int getScoreByActivityAt(Long userId, ActivityAt activityAt) {
-    return reportQueryService.getScoreByActivityAt(userId, activityAt);
+    return toiletScoreService.getScoreByActivityAt(userId, activityAt);
   }
 }
