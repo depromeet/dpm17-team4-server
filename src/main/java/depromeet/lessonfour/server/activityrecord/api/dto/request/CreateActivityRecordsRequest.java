72d9abdf6351c65b6ba818b6defd2121dfb7c884
Koo <koosco136@gmail.com>
2025-10-23 20:08:35 +0900

    update validation message for water intake and change constraint to PositiveOrZero

--- src/main/java/depromeet/lessonfour/server/activityrecord/api/dto/request/CreateActivityRecordsRequest.java
+++ src/main/java/depromeet/lessonfour/server/activityrecord/api/dto/request/CreateActivityRecordsRequest.java
@@ -11,12 +11,12 @@ import depromeet.lessonfour.server.activityrecord.domain.vo.MealTime;
 import depromeet.lessonfour.server.activityrecord.domain.vo.StressLevel;
 import depromeet.lessonfour.server.common.annotation.ValidEnum;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record CreateActivityRecordsRequest(
     @Valid List<CreateFoodRequestDto> foods,
-    @Min(value = 0, message = "마신 물의 잔 수는 양수여야 합니다") int water,
+    @PositiveOrZero(message = "마신 물의 잔 수는 0 이상이어야 합니다") int water,
     @NotNull(message = "스트레스 지수는 필수입니다") @ValidEnum(enumClass = StressLevel.class, message = "유효하지 않은 스트레스 지수입니다")
         StressLevel stress,
     @NotNull(message = "선택한 날짜와 시간은 필수입니다") @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
