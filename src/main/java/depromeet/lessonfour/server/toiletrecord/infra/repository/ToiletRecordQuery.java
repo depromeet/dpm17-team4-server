82bb83d257faa1c216193f1fb7e30092c23151b3
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-10-23 09:50:23 +0900

    [#calendar-toilet] feat(ToiletRecordQuery): findByDate에 is_deleted=false 조건/시간 오름차순 정렬 적용 & findTimesByDate 추가

--- src/main/java/depromeet/lessonfour/server/toiletrecord/infra/repository/ToiletRecordQuery.java
+++ src/main/java/depromeet/lessonfour/server/toiletrecord/infra/repository/ToiletRecordQuery.java
@@ -3,15 +3,20 @@ package depromeet.lessonfour.server.toiletrecord.infra.repository;
 import static depromeet.lessonfour.server.toiletrecord.domain.entity.QToiletRecord.toiletRecord;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import depromeet.lessonfour.server.common.domain.vo.ActivityAt;
 import depromeet.lessonfour.server.common.domain.vo.DailyExistence;
+import depromeet.lessonfour.server.common.domain.vo.RecordTime;
 import depromeet.lessonfour.server.common.infra.DailyExistenceProjection;
+import depromeet.lessonfour.server.common.infra.RecordTimeProjection;
 import depromeet.lessonfour.server.toiletrecord.domain.entity.ToiletRecord;
 import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
@@ -30,11 +35,32 @@ public class ToiletRecordQuery {
                 toiletRecord.user.id.eq(userId),
                 toiletRecord.activityAt.date.eq(activityAt.toDate()),
                 toiletRecord.isDeleted.isFalse())
-            .orderBy(toiletRecord.activityAt.date.asc(), toiletRecord.activityAt.time.asc())
+            .orderBy(toiletRecord.activityAt.time.asc())
             .fetch();
 
-    return records;
+    return records == null ? List.of() : records;
   }
+
+	public List<RecordTime> findTimesByDate(Long userId, ActivityAt activityAt) {
+		List<Tuple> recordTimes =
+			queryFactory
+				.select(toiletRecord.id, toiletRecord.activityAt.time)
+				.from(toiletRecord)
+				.where(
+					toiletRecord.user.id.eq(userId),
+					toiletRecord.activityAt.date.eq(activityAt.toDate()),
+					toiletRecord.isDeleted.isFalse())
+				.orderBy(toiletRecord.activityAt.time.asc())
+				.fetch();
+
+		return recordTimes.stream()
+			.map(
+				t ->
+					new RecordTimeProjection(
+						t.get(toiletRecord.id), t.get(toiletRecord.activityAt.time)))
+			.map(RecordTime.class::cast)
+			.toList();
+	}
 
   public List<DailyExistence> findDailyExistencesBetween(
       Long userId, ActivityAt startInclude, @Nullable ActivityAt endInclude) {
