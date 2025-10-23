6ca27dbb3ca3bc22bc9750e6a7657f97599632e8
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-10-23 11:52:59 +0900

    [#toilet] chore: spotless 적용

--- src/main/java/depromeet/lessonfour/server/toiletrecord/app/dto/request/ToiletRecordCreateRequestDto.java
+++ src/main/java/depromeet/lessonfour/server/toiletrecord/app/dto/request/ToiletRecordCreateRequestDto.java
@@ -13,8 +13,10 @@ import jakarta.validation.constraints.NotNull;
 public record ToiletRecordCreateRequestDto(
     @NotNull(message = "배변시각 기록은 필수입니다.") LocalDateTime occurredAt,
     @NotNull(message = "배변 여부 기록은 필수입니다.") Boolean isSuccessful,
-    @Nullable @ValidEnum(enumClass = ToiletColor.class, message = "유효하지 않은 배변 색깔입니다.") ToiletColor color,
-    @Nullable @ValidEnum(enumClass = ToiletShape.class, message = "유효하지 않은 배변 형태입니다.") ToiletShape shape,
+    @Nullable @ValidEnum(enumClass = ToiletColor.class, message = "유효하지 않은 배변 색깔입니다.")
+        ToiletColor color,
+    @Nullable @ValidEnum(enumClass = ToiletShape.class, message = "유효하지 않은 배변 형태입니다.")
+        ToiletShape shape,
     @Min(value = 0, message = "복통 점수는 0 이상이어야 합니다.")
         @Max(value = 100, message = "복통 점수는 100 이하여야 합니다.")
         int pain,
