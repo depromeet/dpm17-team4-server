22fe6445a4c83fe1f387c19de9bf35f5a5fdcb25
Koo <koosco136@gmail.com>
2025-11-07 02:40:18 +0900

    refactor ScoreSummary to use jakarta.annotation.Nullable for consistency

--- src/main/java/depromeet/lessonfour/server/report/domain/vo/monthly/ScoreSummary.java
+++ src/main/java/depromeet/lessonfour/server/report/domain/vo/monthly/ScoreSummary.java
@@ -1,7 +1,6 @@
 package depromeet.lessonfour.server.report.domain.vo.monthly;
 
-import javax.annotation.Nullable;
-
 import depromeet.lessonfour.server.report.domain.entity.ToiletScore;
+import jakarta.annotation.Nullable;
 
 public record ScoreSummary(@Nullable ToiletScore best, @Nullable ToiletScore worst) {}
