54d9abef1cf45a98dd08c3bc84ab5435e29585a0
Koo <koosco136@gmail.com>
2025-11-07 02:00:14 +0900

    add localized values to DayPeriod, ToiletColor, and ToiletShape enums

--- src/main/java/depromeet/lessonfour/server/report/domain/vo/monthly/DayPeriod.java
+++ src/main/java/depromeet/lessonfour/server/report/domain/vo/monthly/DayPeriod.java
@@ -1,7 +1,14 @@
 package depromeet.lessonfour.server.report.domain.vo.monthly;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum DayPeriod {
-  MORNING,
-  AFTERNOON,
-  EVENING
+  MORNING("오전"),
+  AFTERNOON("오후"),
+  EVENING("저녁");
+
+  private final String value;
 }
--- src/main/java/depromeet/lessonfour/server/toiletrecord/domain/vo/ToiletColor.java
+++ src/main/java/depromeet/lessonfour/server/toiletrecord/domain/vo/ToiletColor.java
@@ -6,18 +6,21 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ToiletColor {
-  DEFAULT,
-  GOLD,
-  DARK_BROWN,
-  RED,
-  GREEN,
-  GRAY;
+  DEFAULT("갈색"),
+  GOLD("황금색"),
+  DARK_BROWN("갈색"),
+  RED("적색"),
+  GREEN("녹색"),
+  GRAY("흰색"),
+  BLACK("흑색");
+
+  private final String value;
 
   public int getScore() {
     return switch (this) {
       case DEFAULT, GOLD -> 0;
       case DARK_BROWN -> -5;
-      case RED, GREEN, GRAY -> -20; // 비정상
+      case RED, GREEN, GRAY, BLACK -> -20; // 비정상
     };
   }
 }
