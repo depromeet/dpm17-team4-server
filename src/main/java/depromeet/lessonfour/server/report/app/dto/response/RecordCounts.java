9d02745950c09dc6e19ed0c07677d4574994aed0
Koo <koosco136@gmail.com>
2025-11-07 02:40:14 +0900

    validate totalCount in RecordCounts constructor to ensure consistency

--- src/main/java/depromeet/lessonfour/server/report/app/dto/response/RecordCounts.java
+++ src/main/java/depromeet/lessonfour/server/report/app/dto/response/RecordCounts.java
@@ -1,6 +1,15 @@
 package depromeet.lessonfour.server.report.app.dto.response;
 
+import depromeet.lessonfour.server.common.api.code.ErrorCode;
+import depromeet.lessonfour.server.common.exception.ServerException;
+
 public record RecordCounts(int activityCount, int toiletCount, int totalCount) {
+
+  public RecordCounts {
+    if (totalCount != activityCount + toiletCount) {
+      throw new ServerException(ErrorCode.INVALID_FIELD_ERROR);
+    }
+  }
 
   public static RecordCounts of(int activityCount, int toiletCount) {
     return new RecordCounts(activityCount, toiletCount, activityCount + toiletCount);
