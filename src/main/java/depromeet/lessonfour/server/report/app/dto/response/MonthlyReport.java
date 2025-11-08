77dcc724dde5259d24a0e3d7e10f1cd08d8ddc11
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-11-09 03:13:03 +0900

    [weekly] feat: 월간리포트 관련 vo 생성

--- src/main/java/depromeet/lessonfour/server/report/app/dto/response/MonthlyReport.java
+++ src/main/java/depromeet/lessonfour/server/report/app/dto/response/MonthlyReport.java
@@ -3,6 +3,7 @@ package depromeet.lessonfour.server.report.app.dto.response;
 import java.util.List;
 
 import depromeet.lessonfour.server.report.domain.vo.Suggestion;
+import depromeet.lessonfour.server.report.domain.vo.monthly.MonthlyActivityReport;
 import depromeet.lessonfour.server.report.domain.vo.monthly.ToiletPainDistribution;
 import depromeet.lessonfour.server.report.domain.vo.monthly.ToiletPeriodCount;
 import depromeet.lessonfour.server.report.domain.vo.monthly.ToiletTimeDistribution;
@@ -10,9 +11,13 @@ import depromeet.lessonfour.server.report.domain.vo.monthly.ToiletTimeDistributi
 public record MonthlyReport(
     RecordCounts recordCounts,
     MonthlyScore monthlyScore,
+    double averageScore,
+    List<Integer> weeklyAverageScores,
     List<ToiletShapeCount> shape,
     ToiletTimeDistribution timeDistribution,
     List<ToiletColorCount> color,
     ToiletPainDistribution pain,
     List<ToiletPeriodCount> timeOfDay,
-    Suggestion suggestion) {}
+    Suggestion suggestion,
+    // 월간 Activity 원본 (주차 그룹 포함)
+    MonthlyActivityReport activityReport) {}
