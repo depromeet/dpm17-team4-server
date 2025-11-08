8db1606c745aa9f69207695f800f647b2fc4b728
ihnokim <ihnokim58@gmail.com>
2025-11-08 10:07:43 +0900

    Add messages (#113)

--- src/main/java/depromeet/lessonfour/server/report/api/mapper/StressMapper.java
+++ src/main/java/depromeet/lessonfour/server/report/api/mapper/StressMapper.java
@@ -10,12 +10,21 @@ public class StressMapper {
 
   public StressReport map(StressEvaluation stressEvaluation) {
     return switch (stressEvaluation) {
-      case VERY_LOW, LOW, MEDIUM -> new StressReport(
-          "긍정적인 당신, 칭찬해요!\n그 마인드 오래도록 유지해봐요",
-          "https://kr.object.ncloudstorage.com/depromeet-dev-static-resources/condition_good.png");
-      case HIGH, VERY_HIGH -> new StressReport(
-          "스트레스 관리가 필요해요.\n가벼운 산책이나 명상은 어때요?",
+      case VERY_LOW -> new StressReport(
+          "삐용삐용! 스트레스 만땅! 산책이나 명상을 해볼까요?",
+          "https://kr.object.ncloudstorage.com/depromeet-dev-static-resources/condition_very_bad.png");
+      case LOW -> new StressReport(
+          "스트레스에 잡아먹히지 않도록 조심해봐요!",
           "https://kr.object.ncloudstorage.com/depromeet-dev-static-resources/condition_bad.png");
+      case MEDIUM -> new StressReport(
+          "스트레스 발생! 마음을 다스릴 수 있도록 노력해요",
+          "https://kr.object.ncloudstorage.com/depromeet-dev-static-resources/condition_medium.png");
+      case HIGH -> new StressReport(
+          "긍정적인 당신! 그 마인드 오래도록 유지해봐요",
+          "https://kr.object.ncloudstorage.com/depromeet-dev-static-resources/condition_good.png");
+      case VERY_HIGH -> new StressReport(
+          "스트레스 Zero! 행복한 하루가 되셨군요?",
+          "https://kr.object.ncloudstorage.com/depromeet-dev-static-resources/condition_very_good.png");
       case NONE -> null;
     };
   }
