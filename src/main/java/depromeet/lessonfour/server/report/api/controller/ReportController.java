2beeca95a1bb955b4f08ab1f336ed31abd208f1b
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-11-09 03:56:58 +0900

    [weekly] feat(ReportController): 주간, 리포트 엔드포인트 추가

--- src/main/java/depromeet/lessonfour/server/report/api/controller/ReportController.java
+++ src/main/java/depromeet/lessonfour/server/report/api/controller/ReportController.java
@@ -15,12 +15,16 @@ import depromeet.lessonfour.server.common.api.code.SuccessCode;
 import depromeet.lessonfour.server.common.api.dto.SuccessResponse;
 import depromeet.lessonfour.server.report.api.dto.response.GetDailyReportResponseDto;
 import depromeet.lessonfour.server.report.api.dto.response.GetMonthlyReportResponseDto;
+import depromeet.lessonfour.server.report.api.dto.response.GetWeeklyReportResponseDto;
 import depromeet.lessonfour.server.report.api.mapper.DailyReportMapper;
 import depromeet.lessonfour.server.report.api.mapper.MonthlyReportMapper;
+import depromeet.lessonfour.server.report.api.mapper.WeeklyReportMapper;
 import depromeet.lessonfour.server.report.app.dto.response.DailyReport;
 import depromeet.lessonfour.server.report.app.dto.response.MonthlyReport;
+import depromeet.lessonfour.server.report.app.dto.response.WeeklyReport;
 import depromeet.lessonfour.server.report.app.service.GetDailyReportUseCase;
 import depromeet.lessonfour.server.report.app.service.MonthlyReportUseCase;
+import depromeet.lessonfour.server.report.app.service.WeeklyReportUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -32,8 +36,10 @@ import lombok.RequiredArgsConstructor;
 public class ReportController {
 
   private final GetDailyReportUseCase getDailyReportUseCase;
+  private final WeeklyReportUseCase weeklyReportUseCase;
   private final MonthlyReportUseCase monthlyReportUseCase;
   private final DailyReportMapper dailyReportMapper;
+  private final WeeklyReportMapper weeklyReportMapper;
   private final MonthlyReportMapper monthlyReportMapper;
   private final Clock clock;
 
@@ -46,6 +52,17 @@ public class ReportController {
     DailyReport dailyReport = getDailyReportUseCase.getDailyReport(userId, baseDateTime);
     return SuccessResponse.of(
         SuccessCode.SUCCESS_FETCH, dailyReportMapper.map(dailyReport, baseDateTime));
+  }
+
+  @Operation(summary = "주간 리포트 조회", description = "특정 주의 주간 리포트를 조회합니다.")
+  @GetMapping("/weekly")
+  public SuccessResponse<GetWeeklyReportResponseDto> getWeeklyReport(
+      @AuthenticationPrincipal(expression = "id") Long userId,
+      @RequestParam(required = false) LocalDateTime dateTime) {
+    LocalDateTime baseDateTime = (dateTime != null) ? dateTime : LocalDateTime.now(clock);
+    WeeklyReport weeklyReport = weeklyReportUseCase.generateWeeklyReport(userId, baseDateTime);
+    return SuccessResponse.of(
+        SuccessCode.SUCCESS_FETCH, weeklyReportMapper.map(weeklyReport, baseDateTime));
   }
 
   @Operation(summary = "월간 리포트 조회", description = "특정 달의 월간 리포트를 조회합니다.")
