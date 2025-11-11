90880d979c89bf25778febe7407e4190bfb1326c
Koo <koosco136@gmail.com>
2025-11-12 02:08:47 +0900

    rename GetDailyReportUseCase to DailyReportUseCase and update references in ReportController

--- src/main/java/depromeet/lessonfour/server/report/api/controller/ReportController.java
+++ src/main/java/depromeet/lessonfour/server/report/api/controller/ReportController.java
@@ -19,7 +19,7 @@ import depromeet.lessonfour.server.report.api.dto.response.GetWeeklyReportRespon
 import depromeet.lessonfour.server.report.api.mapper.DailyReportMapper;
 import depromeet.lessonfour.server.report.api.mapper.MonthlyReportMapper;
 import depromeet.lessonfour.server.report.api.mapper.WeeklyReportMapper;
-import depromeet.lessonfour.server.report.app.service.GetDailyReportUseCase;
+import depromeet.lessonfour.server.report.app.service.DailyReportUseCase;
 import depromeet.lessonfour.server.report.app.service.MonthlyReportUseCase;
 import depromeet.lessonfour.server.report.app.service.WeeklyReportUseCase;
 import depromeet.lessonfour.server.report.domain.vo.DailyReport;
@@ -35,9 +35,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ReportController {
 
-  private final GetDailyReportUseCase getDailyReportUseCase;
+  private final DailyReportUseCase dailyReportUseCase;
   private final WeeklyReportUseCase weeklyReportUseCase;
   private final MonthlyReportUseCase monthlyReportUseCase;
+
   private final DailyReportMapper dailyReportMapper;
   private final WeeklyReportMapper weeklyReportMapper;
   private final MonthlyReportMapper monthlyReportMapper;
@@ -51,7 +52,7 @@ public class ReportController {
 
     LocalDateTime baseDateTime = (dateTime != null) ? dateTime : LocalDateTime.now(clock);
 
-    DailyReport dailyReport = getDailyReportUseCase.getDailyReport(userId, baseDateTime);
+    DailyReport dailyReport = dailyReportUseCase.getDailyReport(userId, baseDateTime);
 
     return SuccessResponse.of(
         SuccessCode.SUCCESS_FETCH, dailyReportMapper.map(dailyReport, baseDateTime));
