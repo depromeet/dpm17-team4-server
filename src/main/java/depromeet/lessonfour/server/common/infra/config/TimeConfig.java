0ef1c4354894c631f015a0ff9fd53d92e06381ee
ihnokim <ihnokim58@gmail.com>
2025-11-15 06:07:22 +0900

    Fix timezone bug (#132)

--- src/main/java/depromeet/lessonfour/server/common/infra/config/TimeConfig.java
+++ src/main/java/depromeet/lessonfour/server/common/infra/config/TimeConfig.java
@@ -1,6 +1,7 @@
 package depromeet.lessonfour.server.common.infra.config;
 
 import java.time.Clock;
+import java.time.ZoneId;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,6 @@ public class TimeConfig {
 
   @Bean
   public Clock clock() {
-    return Clock.systemUTC();
+    return Clock.system(ZoneId.of("Asia/Seoul"));
   }
 }
